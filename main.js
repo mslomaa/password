ddc932222c62a0e3047b773ccd772a2a7f4c0d2f
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-30 22:13:09 +0200

    modify js

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
-const input = document.getElementById('pass');
+// const input = document.getElementById('pass');
 
-const div = document.querySelector('.message')
-const passwords = ['user', 'spring', 'autumn']
-const messages = ['krolik', 'zajac', 'zwierzak']
+// const div = document.querySelector('.message')
+// const passwords = ['user', 'spring', 'autumn']
+// const messages = ['krolik', 'zajac', 'zwierzak']
 
-input.addEventListener('input', (e) => {
-    div.textContent = '';
-    const text = e.target.value;
+// input.addEventListener('input', (e) => {
+//     div.textContent = '';
+//     const text = e.target.value;
 
-    passwords.forEach((password, index)=> {
-        if(password === text){
-            div.textContent = messages[index];
-            e.target.value = '';
+//     passwords.forEach((password, index)=> {
+//         if(password === text){
+//             div.textContent = messages[index];
+//             e.target.value = '';
             
-        }
+//         }
+//     })
+
+// })
+
+
+// input.addEventListener('focus', (e) => {
+//     e.target.classList.add('active');
+// })
+
+
+// input.addEventListener('blur', (e) => {
+//     e.target.classList.remove('active');
+// })
+
+
+const input = document.querySelector('input');
+const mess = document.querySelector('.message')
+const passwords = ['jedEN', 'DwA'];
+const messages = ['super', 'dziala'];
+
+const showMessage = (e) => {
+    const text = e.target.value.toLowerCase();
+    passwords.forEach((password, index) => {
+        let newPass = password.toLowerCase();
+        if(newPass === text){
+            mess.textContent = messages[index];
+            e.target.value = ''
+        }    
+        
     })
 
-})
+}
 
-
-input.addEventListener('focus', (e) => {
-    e.target.classList.add('active');
-})
-
-
-input.addEventListener('blur', (e) => {
-    e.target.classList.remove('active');
-})
-
-// if(password === e.target.value){
-//     div.textContent = message;
-//     e.target.value = '';
-// }else {
-//     div.textContent = '';
-// }
+input.addEventListener('input', showMessage)
